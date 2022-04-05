144a6fbb4f43c828ae8dd5aeb918ec16e39bbb77
Somprasong Damyos <somprasong.damyos@gmail.com>
2022-04-05 17:02:12 +0700

    remove unused

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -3,7 +3,7 @@ import PageTitle from '@/components/PageTitle'
 import generateRss from '@/lib/generate-rss'
 import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { formatSlug, getAllFilesFrontMatter, getFileBySlug, getFiles } from '@/lib/mdx'
-import generateFeaturedImage from '@/lib/generate-featured-image'
+// import generateFeaturedImage from '@/lib/generate-featured-image'
 
 const DEFAULT_LAYOUT = 'PostLayout'
 
